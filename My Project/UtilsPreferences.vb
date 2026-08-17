ed60ceccf40fffd41292e1f6dd54649ccdcdced5
JBen79 <juri.bendoni@gmail.com>
2026-08-17 17:23:39 +0200

    Register Finalize model and drawings task

--- My Project/UtilsPreferences.vb
+++ My Project/UtilsPreferences.vb
@@ -987,6 +987,8 @@ Public Class UtilsPreferences
         If Splash IsNot Nothing Then Splash.UpdateStatus(TaskList(TaskList.Count - 1).Name)
         TaskList.Add(New TaskFitView)
         If Splash IsNot Nothing Then Splash.UpdateStatus(TaskList(TaskList.Count - 1).Name)
+        TaskList.Add(New TaskFinalizeModelAndDrawings)
+        If Splash IsNot Nothing Then Splash.UpdateStatus(TaskList(TaskList.Count - 1).Name)
 
         ' Check
         TaskList.Add(New TaskCheckInterference)
